b34bbe8379e919a56979017976be6512fed6619b
camillazicari <camyzicari01@gmail.com>
2024-12-17 15:27:16 +0100

    creato print delle canzoni

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+let artist;
+const coverRandom = document.getElementById('coverRandom');
+const albumRandom = document.getElementById('albumRandom');
+const titoloRandom = document.getElementById('titoloRandom');
+const artistaRandom = document.getElementById('artistaRandom');
+const btnPlayRandom = document.getElementById('btnPlayRandom');
+
+const artista = Math.floor(Math.random() * 10)+1;
+const url = `https://striveschool-api.herokuapp.com/api/deezer/artist/${artista}/top?limit=1`;
+
+async function getArtist() {
+    try {
+        let response = await fetch(url);        
+        data = await response.json();
+        artist = data;        
+        console.log('RISPOSTA:', artist);
+        printSong();
+    } catch (error) {
+        console.log(error);
+    }
+}
+function printSong() {
+    coverRandom.setAttribute('src', artist.data[0].album.cover_medium);
+    albumRandom.innerHTML = artist.data[0].album.title;
+    titoloRandom.innerHTML = artist.data[0].title;
+    artistaRandom.innerHTML = artist.data[0].artist.name;
+}
+
+getArtist();
